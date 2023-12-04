bea49048b16ab384bbf79e3dea441df09bcdb64e
joshua.smee <joshua.smee@thetradedesk.com>
2023-12-04 18:57:29 +1100

    Part 1

---
+++ day4/Card.cs
@@ -0,0 +1,40 @@
+namespace day4;
+
+public class Card
+{
+    public int[] winning;
+    public int[] numbers;
+
+    public Card(int[] winning, int[] numbers)
+    {
+        this.winning = winning;
+        this.numbers = numbers;
+    }
+
+    public static Card ParseString(String input)
+    {
+        string[] parts = input.Split('|');
+
+        return new Card(ParseList(parts[0]), ParseList(parts[1]));
+    }
+
+    private static int[] ParseList(String input)
+    {
+        input = input.Trim();
+        string[] numbers = input.Split(' ').Where(s => s != "" ).ToArray();
+        
+        return numbers.Select(s => Int32.Parse(s.Trim())).ToArray();
+    }
+
+    public int GetPoints()
+    {
+        int numWinning = numbers.Count(i => winning.Contains(i));
+                
+        if (numWinning == 0)
+        {
+            return 0;
+        }
+
+        return (int)Math.Pow(2, numWinning - 1);
+    }
+}
---
+++ day4/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using day4;
+
+string[] lines = File.ReadAllLines("input.txt");
+
+int[] scores = lines.Select(s =>
+{
+    String input = s.Split(':')[1];
+    Console.WriteLine(input);
+    Card card = Card.ParseString(input);
+    return card.GetPoints();
+}).ToArray();
+
+int score = scores.Sum();
+
+Console.WriteLine(score);
